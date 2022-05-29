ffa9af8b991b7ec4725c6ce404ab868e4fdf99a0
heyethereum <heyethereum@gmail.com>
2022-05-29 12:54:16 +0800

    added test scripts

--- smart-contracts/test/sample-test.js
+++ smart-contracts/test/sample-test.js
@@ -2,7 +2,7 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Greeter", function () {
-  it("Should retrun the new greeting once it's changed", async function () {
+  it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello Kitties!");
     await greeter.deployed();
